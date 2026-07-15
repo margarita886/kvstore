73d1de438b5439e56dff30870719d7c600b59043
Margarita <margaritagolovina34@gmail.com>
2026-07-15 23:16:34 +0300

    Stage 3: networked seever(linux), client connection using TCP

--- kvstore.cpp
+++ kvstore.cpp
@@ -3,29 +3,51 @@
 #include<string>
 #include<fstream>
 #include <map>
+#include <sys/socket.h>
+#include <netinet/in.h>
 using namespace std;
 
 int main(){
 
+
 map<string, string> data;
 string line;
 
-ifstream inFile("data.txt");
-string fileLine;
 
-while(getline(inFile, fileLine)){
-    stringstream fs(fileLine);
-    string k, v;
-    getline(fs, k, ',');
-    getline(fs, v, ',');
-    data[k] = v;
+int server_fd = socket(AF_INET, SOCK_STREAM, 0); //create socket
+if(server_fd==-1){
+    cout<<"Failed to create socket";
 }
 
-bool ex = false;
+sockaddr_in address; //setting address and port
+address.sin_family = AF_INET;  //ipv4
+address.sin_addr.s_addr = INADDR_ANY;
+address.sin_port = htons(8080); //set port
 
+
+int bind_result = bind(server_fd, (sockaddr*)&address, sizeof(address));
+if (bind_result == -1) {
+    cout << "Failed to bind";
+}
+
+int lis_res = listen(server_fd, 5); //5 connections allowed - listen to request
+if (lis_res == -1){
+    cout<<"Fail to listen";
+}
+int client_fd = accept(server_fd, nullptr, nullptr); //server fd is the socket thats listening
+if(client_fd == -1){ //client fd is the scoket for the clinet
+    cout<<"Connection acceptance failed";
+}
+else
+    cout<<"Client connected succesfully"<<endl;
+
+bool ex = false;
 while(ex==false){
 
-    getline(cin , line);
+    char buffer[1024] = {0};
+    recv(client_fd, buffer, 1024, 0);
+    string line(buffer);
+
     stringstream ss(line);
     string com;
     ss >> com;
@@ -40,12 +62,13 @@ while(ex==false){
     else if(com=="GET"){
         auto it = data.find(key);
         if(it == data.end()){
-            cout<<"KEY NOT FOUND. USE 'SET' to set a new key"<<endl;
+            string response = "KEY NOT FOUND. USE 'SET' to set a new key\n";
+            send(client_fd, response.c_str(), response.size(), 0);
         } 
     else{
-        cout<<it->second<<endl;
+        string response = it->second + "\n";
+        send(client_fd, response.c_str(), response.size(), 0);
     }
-
     }
     else if(com=="DELETE"){
         data.erase(key);
@@ -54,15 +77,26 @@ while(ex==false){
     else if(com=="EXIT"){
         ex=true;
     }
-
-
 }
+ 
+ifstream inFile("data.txt");
+string fileLine;
+//read into file
+while(getline(inFile, fileLine)){
+    stringstream fs(fileLine);
+    string k, v;
+    getline(fs, k, ',');
+    getline(fs, v, ',');
+    data[k] = v;
+}
+
+
+//write into file
 
 ofstream outFile("data.txt");
 for(pair<string,string> entry : data){
     outFile<<entry.first<<","<<entry.second<<endl;
 }
 outFile.close();
-
 return 0;    
 }
